3943f887768589947a24f0dc8aa5146f7561985f
Rakshit <rakshit087@gmail.com>
2022-04-28 19:09:05 +0530

    Updated Backend to Include Model

--- app.py
+++ app.py
@@ -1,27 +1,28 @@
 from flask import Flask
 from flask import request
-import numpy as np
+from flask import jsonify
+from flask_cors import CORS
 import tensorflow as tf
-
+import numpy as np
+import requests
 app = Flask(__name__)
+CORS(app)
 
-model_a = tf.keras.models.load_model("./models/model_a.pb")
-model_v = tf.keras.models.load_model("./models/model_v.pb")
-model_d = tf.keras.models.load_model("./models/model_d.pb")
+@app.route("/mood")
+#Get the mood with the help of device
+def mood():
+    data = requests.get("http://192.168.43.86").content
+    data = data.split()
+    data = np.array(data, dtype=np.float64).reshape(-1, 42)
+    model_a = tf.keras.models.load_model('saved_model/modev_a')
+    model_v = tf.keras.models.load_model('saved_model/modev_v')
+    model_d = tf.keras.models.load_model('saved_model/modev_d')
+    valence = model_v.predict(data)
+    dominance = model_d.predict(data)
+    arousal = model_a.predict(data)
+    response = jsonify(
+        str([valence[0][0], dominance[0][0], arousal[0][0]]))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-@app.route("/predict",methods = ['GET'])
-def predictEmotion():
-    if request.method == 'GET':
-        data = request.args.get('data','')
-        data = data.split(',')
-        data = np.array(data, dtype = np.float64).reshape(-1,68 )
-
-        valence = model_v.predict(data)
-        dominance = model_d.predict(data)
-        arousal = model_a.predict(data)
-        return str([valence[0],dominance[0],arousal[0]])
 app.run()
